6627eec5865e274d2014954244e3409729a20c03
Lina Adamsson <studio@adamsson.com>
2023-08-03 08:52:19 +0200

    Added components and installed npm

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,14 @@
+// import React, { useEffect, useState } from 'react';
 import React from 'react';
+import Header from 'components/Header';
+// import NewThoughts from 'components/NewThoughts';
+import ThoughtsList from 'components/ThoughtsList';
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
+      <ThoughtsList />
     </div>
   );
 }
---
+++ code/src/components/ThoughtsList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ThoughtsList = () => {
+  return <div>Thoughts list</div>
+}
+
+export default ThoughtsList;
