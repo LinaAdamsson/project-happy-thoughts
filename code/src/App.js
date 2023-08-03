690311d35f01ce4604db260178e2a271b7b18c5e
Lina Adamsson <studio@adamsson.com>
2023-08-03 11:13:22 +0200

    Input field now accessible, submit button works. New message not yet showing up. Heart button not yet adding new likes.

--- code/src/App.js
+++ code/src/App.js
@@ -45,6 +45,8 @@ export const App = () => {
   const onFormSubmit = (event) => {
     event.preventDefault();
 
+    // Below: Send the POST request with the input from your form
+
     const options = {
       method: 'POST',
       headers: {
@@ -90,3 +92,17 @@ export const App = () => {
     </div>
   )
 };
+
+//     fetch('https://technigo-thoughts.herokuapp.com/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//     message: 'Hello world' })
+//   })
+//     .then((res) => res.json())
+//     .then((newThought) => {
+//       // Now you have `newThought` which is the response from the
+//       // API as documented at the top of this readme. You can use
+//       // it to update the `thoughts` array:
+//       setThoughts((previousThoughts) => [newThought, ...previousThoughts])
+//     })
+// }
--- code/src/components/NewThoughts.js
+++ code/src/components/NewThoughts.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NewThoughts = ({ newMessage, onNewThoughtsChange, onFormSubmit }) => {
+const NewThoughts = ({ newMessage, handleNewThoughtsChange, onFormSubmit }) => {
+  const [remainingChars, setRemainingChars] = useState(160 - newMessage.length);
+  // Above: newMessage = a state passed in as a prop (to component)
+  const handleInputChange = (event) => {
+    const input = event.target.value;
+    const remaining = 160 - input.length;
+    setRemainingChars(remaining);
+    handleNewThoughtsChange(event);
+  };
+  // Above: handleNewThoughtsChange = Function called when writing message in text field
+
+  // Below: onFormSubmit when user submits the form.
   return (
-    <form onSubmit={onFormSubmit}>
-      <h1>Welcome to Happy Thoughts app! Type new task below.</h1>
-      <textarea value={newMessage} onChange={onNewThoughtsChange} />
-      <button type="submit">Submit form!</button>
+    <form className="message-container" onSubmit={onFormSubmit}>
+      <p className="thought-text">Share a happy thought!</p>
+      <textarea
+        placeholder="What's on your mind?"
+        value={newMessage}
+        onChange={handleInputChange} />
+      <div className="main">
+        <div className="char-count">{remainingChars} characters remaining</div>
+        <button className="submit-btn" type="submit" disabled={newMessage.length < 1 || newMessage.length > 160}>
+          ❤️Send a Happy Thought❤️
+        </button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default NewThoughts;
+export default NewThoughts
